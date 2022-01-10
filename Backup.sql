724dd9c947ac3e940cffd494de9f7e838c8eab86
JayaByu <jayawardhanabayu@gmail.com>
2022-01-10 16:23:29 +0700

    Kurang Fitur

--- Backup.sql
+++ Backup.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE Bansos;
-USE Bansos;
+CREATE DATABASE x00;
+USE x00;
 DROP DATABASE test;
 
 CREATE TABLE Pempu(
@@ -103,6 +103,9 @@ CREATE TABLE Kurir(
     ),
 );
 
+DROP TABLE Bank;
+
+
 
 CREATE TABLE Bank(
     IdBank CHAR(6) NOT NULL,
@@ -113,10 +116,6 @@ CREATE TABLE Bank(
     NamaBank VARCHAR(255),
     Transaksi DATE,
     PRIMARY KEY (IdBank),
-    FOREIGN KEY (IdPempu) 
-        REFERENCES Pempu(IdPusat) ON DELETE CASCADE ON UPDATE CASCADE,
-    FOREIGN KEY (IdPemda)
-        REFERENCES Pemda(IdPemda) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (IdPenerima)
         REFERENCES Masyarakat(IdMasya) ON DELETE CASCADE ON UPDATE CASCADE,
     FOREIGN KEY (IdBansos)
@@ -127,6 +126,8 @@ CREATE TABLE Bank(
         )
     )
 ); 
+
+DROP TABLE Ekspedisi;
 
 
 CREATE TABLE Ekspedisi(
@@ -159,3 +160,154 @@ CREATE TABLE Ekspedisi(
 
 
 
+INSERT INTO Pempu(
+    IdPusat,
+    NamaPempus,
+    Gender,
+    Alamat,
+    Phone
+) VALUES (
+    'PU001',
+    'Sukijan',
+    'Laki',
+    'Kompleks Kantor Gubernur, Jl. Soa Siu Dok 2, Jayapura, Papua.',
+    '(0967)537523'
+),(
+    'PU002',
+    'Sukimin',
+    'Perempuan',
+    'Jl. Bumijo No.5, Bumijo, Jetis, Kota Yogyakarta, Daerah Istimewa Yogyakarta.',
+    '(0274)589074'
+);
+
+INSERT INTO Pemda(
+    IdPemda,
+    NamaPemda,
+    Gender,
+    Alamat,
+    Phone
+) VALUES (
+    'PD001',
+    'Suparno',
+    'Laki',
+    'Jl. Medan Merdeka Selatan 8-9 Blok G Lantai 3 Jakarta.',
+    '(0642)21399'
+),(
+    'PD002',
+    'Suparni',
+    'Perempuan',
+    'Jl. Jend.Sudirman No.51, Padang.',
+    '(0751)31549'
+);
+
+INSERT INTO Masyarakat(
+    IdMasya,
+    Nama,
+    Gender,
+    Alamat,
+    Phone
+) VALUES (
+    'MS001',
+    'Issei',
+    'Laki',
+    'Jl Kramat Raya 3 H Kompl Maya Indah II, Dki Jakarta.',
+    '02131613390'
+),(
+    'MS002',
+    'Rias',
+    'Perempuan',
+    'Jl Sediyatmo Ged Ranujaya Benda, Jakarta',
+    '02155912755'
+);
+
+INSERT INTO DBKecamatan(
+    IdKecamatan,
+    IdMasyarakat,
+    Kecamatan
+) VALUES (
+    'KC001',
+    'MS001',
+    'Kramat'
+),(
+    'KC002',
+    'MS002',
+    'RanuJoyo'
+);
+
+INSERT INTO TipeBansos(
+    IdBansos,
+    JenisBansos,
+    JumlahBansos
+) VALUES (
+    'TB001',
+    'NonTunai',
+    '2'
+),(
+    'TB002',
+    'Tunai',
+    '2000000'
+);
+
+INSERT INTO Kurir(
+    IdKurir,
+    Nama,
+    Alamat,
+    Gender,
+    Usia
+) VALUES (
+    'KU001',
+    'Supratno',
+    'Jalan Cheras Mewah 10, Taman Cheras Mewah',
+    'Laki',
+    '20th'
+),(
+    'KU002',
+    'Supratman',
+    'Jalan Sulam, Taman Sentosa.',
+    'Laki',
+    '19th'
+);
+
+INSERT INTO Bank(
+    IdBank,
+    IdPempu,
+    IdPemda,
+    IdPenerima,
+    IdBansos,
+    NamaBank,
+    Transaksi
+) VALUES (
+    'BK001',
+    '',
+    'PD001',
+    'MS001',
+    'TB002',
+    'Mandiri',
+    '2022/12/24'
+);
+
+INSERT INTO Ekspedisi(
+    IdEkspedisi,
+    IdPempu,
+    IdPemda,
+    IdKurir,
+    IdPenerima,
+    IdBansos,
+    Harga,
+    JenisBarang,
+    TanggalPengiriman,
+    IdDestinasi
+) VALUES (
+    'EX001',
+    '',
+    'PD002',
+    'KU002',
+    'MS002',
+    'TB001',
+    30000000,
+    'Beras',
+    '2022/1/1',
+    'Jl Sediyatmo Ged Ranujaya Benda, Jakarta'
+);
+
+SELECT * FROM Ekspedisi;
---
+++ insert.sql
@@ -0,0 +1,150 @@
+USE test;
+INSERT INTO Pempu(
+    IdPusat,
+    NamaPempus,
+    Gender,
+    Alamat,
+    Phone
+) VALUES (
+    'PU001',
+    'Sukijan',
+    'Laki',
+    'Kompleks Kantor Gubernur, Jl. Soa Siu Dok 2, Jayapura, Papua.',
+    '(0967)537523'
+),(
+    'PU002',
+    'Sukimin',
+    'Perempuan',
+    'Jl. Bumijo No.5, Bumijo, Jetis, Kota Yogyakarta, Daerah Istimewa Yogyakarta.',
+    '(0274)589074'
+);
+
+INSERT INTO Pemda(
+    IdPemda,
+    NamaPemda,
+    Gender,
+    Alamat,
+    Phone
+) VALUES (
+    'PD001',
+    'Suparno',
+    'Laki',
+    'Jl. Medan Merdeka Selatan 8-9 Blok G Lantai 3 Jakarta.',
+    '(0642)21399'
+),(
+    'PD002',
+    'Suparni',
+    'Perempuan',
+    'Jl. Jend.Sudirman No.51, Padang.',
+    '(0751)31549'
+);
+
+INSERT INTO Masyarakat(
+    IdMasya,
+    Nama,
+    Gender,
+    Alamat,
+    Phone
+) VALUES (
+    'MS001',
+    'Issei',
+    'Laki',
+    'Jl Kramat Raya 3 H Kompl Maya Indah II, Dki Jakarta.',
+    '02131613390'
+),(
+    'MS002',
+    'Rias',
+    'Perempuan',
+    'Jl Sediyatmo Ged Ranujaya Benda, Jakarta',
+    '02155912755'
+);
+
+INSERT INTO DBKecamatan(
+    IdKecamatan,
+    IdMasyarakat,
+    Kecamatan
+) VALUES (
+    'KC001',
+    'MS001',
+    'Kramat'
+),(
+    'KC002',
+    'MS002',
+    'RanuJoyo'
+)
+
+INSERT INTO TipeBansos(
+    IdBansos,
+    JenisBansos,
+    JumlahBansos
+) VALUES (
+    'TB001',
+    'NonTunai',
+    '2'
+),(
+    'TB002',
+    'Tunai',
+    '2000000'
+)
+
+INSERT INTO Kurir(
+    IdKurir,
+    Nama,
+    Alamat,
+    Gender,
+    Usia
+) VALUES (
+    'KU001',
+    'Supratno',
+    'Jalan Cheras Mewah 10, Taman Cheras Mewah',
+    'Laki',
+    '20th'
+),(
+    'KU002',
+    'Supratman',
+    'Jalan Sulam, Taman Sentosa.',
+    'Laki',
+    '19th'
+)
+
+INSERT INTO Bank(
+    IdBank,
+    IdPempu,
+    IdPemda,
+    IdPenerima,
+    IdBansos,
+    NamaBank,
+    Transaksi
+) VALUES (
+    'BK001',
+    '',
+    'PD001',
+    'MS001',
+    'TB002',
+    'Mandiri',
+    '2022/12/24'
+);
+
+INSERT INTO Ekspedisi(
+    IdEkspedisi,
+    IdPempu,
+    IdPemda,
+    IdKurir,
+    IdPenerima,
+    IdBansos,
+    Harga,
+    JenisBarang,
+    TanggalPengiriman,
+    IdDestinasi
+) VALUES (
+    'EX001',
+    '',
+    'PD002',
+    'KU002',
+    'MS002',
+    'TB001',
+    30000000,
+    'Beras',
+    '2022/1/1',
+    'Jl Sediyatmo Ged Ranujaya Benda, Jakarta'
+);
